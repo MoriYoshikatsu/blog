f7680761526f620fe54c655f12fdf5a685357cd6
MoriYoshikatsu <moriyosi36@gmail.com>
2024-04-12 03:39:52 +0900

    modified:   app/Http/Controllers/PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,9 +7,9 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function index(Post $post)
-    {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
+    public function show(Post $post)
+    {   dd($post);
+        return view('posts.show')->with(['post' => $post]);
     }
 }
 ?>
