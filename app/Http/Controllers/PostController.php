18b1faf965f7e59671946cc25b83321de89ad90b
MoriYoshikatsu <moriyosi36@gmail.com>
2024-04-12 04:24:55 +0900

    8-2修正

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function show(Post $post)
-    {   dd($post);
-        return view('posts.show')->with(['post' => $post]);
+    public function index(Post $post)
+    {   
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
 }
 ?>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,12 +10,10 @@
         <h1>Blog Name</h1>
         <div class='posts'>
             @foreach ($posts as $post)
-            <div class='post'>
-                <h2 class='title'>
-                    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                </h2>
-                <p class='body'>{{ $post->body }}</p>
-            </div>
+                <div class='post'>
+                    <h2 class='title'>{{ $post->title }}</h2>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
             @endforeach
         </div>
         <div class='paginate'>
